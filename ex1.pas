3c665ff777a2112f1ce9799a8a8a632474819d7c
Сашенька <138240993+v0untina@users.noreply.github.com>
2023-10-12 14:23:14 +0300

    Add files via upload

---
+++ ex1.pas
@@ -0,0 +1,15 @@
+﻿program ex1;
+var x,y: real;
+begin
+  write ('x= ');
+  read(x);
+  if x<-8 then 
+    y:=cos(2*x)*77
+  else 
+    if x<1 then
+      y:=exp(x)/-x
+    else
+      if 1<=x then
+        y:=power(0,1*x)*tan(x)-cos(2*x);
+   writeln('y= ', y);
+end.
---
+++ ex2.pas
@@ -0,0 +1,15 @@
+﻿program ex2;
+var x: real;
+begin
+   x:=-10;
+while x<=-1 do 
+  begin
+  if x<-8 then 
+    writeln ('y = ',cos(2*x)*77:1:2) else 
+    if (x>=-8) and (x<1) then
+       writeln ('y = ', exp(x)/-x:1:2) else
+      if x>=3 then 
+        writeln ('y = ',power(0,1*x)*tan(x)-cos(2*x):1:2); 
+            x:=x+0.2;
+      end;
+end.
